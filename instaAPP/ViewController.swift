62a6c0d7a756385e32781683613f9d8deb2898d5
masaponpon <masaya.ishigami.ntt@gmail.com>
2018-10-03 22:03:35 +0900

    Initial Commit

---
+++ instaAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaAPP
+//
+//  Created by masapon on 2018/10/03.
+//  Copyright © 2018年 masaya ishigami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
